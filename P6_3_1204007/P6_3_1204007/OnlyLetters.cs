f60da13bc8fd86590f33d9c35e64fabdd044c3c0
Wulan Nur Annisah <wulannurannisah470@gmail.com>
2021-12-10 20:28:11 +0700

    Add files via upload

---
+++ P6_3_1204007/P6_3_1204007/OnlyLetters.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P6_3_1204007
+{
+    public partial class OnlyLetters : UserControl
+    {
+        public OnlyLetters()
+        {
+            InitializeComponent();
+        }
+
+        public override string Text
+        {
+            get
+            {
+                return base.Text;
+            }
+            set
+            {
+                try
+                {
+                    int.Parse(value);
+                    base.Text = value;
+                    return;
+                }
+                catch
+                { }
+                if (value == null)
+                {
+                    base.Text = value;
+                    return;
+                }
+            }
+        }
+
+
+        public delegate void InvalidUserEntryEvent(object sender, KeyPressEventArgs e);
+        public event InvalidUserEntryEvent InvalidUserEntry;
+        private void OnlyLetters_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsLetter(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
